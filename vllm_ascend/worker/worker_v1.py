b05c9d6214c2c31155699f0329b1c7132f1558f4
1092626063 <1092626063@qq.com>
2025-09-26 10:39:41 +0800

    fix aclgraph dummyrun

--- vllm_ascend/worker/worker_v1.py
+++ vllm_ascend/worker/worker_v1.py
@@ -333,7 +333,9 @@ class NPUWorker(WorkerBase):
         return self.model_runner.pin_lora(lora_id)
 
     def execute_dummy_batch(self) -> None:
-        self.model_runner._dummy_run(1)
+        aclgraph_mode = self.vllm_config.compilation_config.cudagraph_mode
+        aclgraph_runtime_mode = aclgraph_mode.mixed_mode()
+        self.model_runner._dummy_run(1, aclgraph_runtime_mode=aclgraph_runtime_mode)
 
     def _init_worker_distributed_environment(self) -> None:
         """Initialize the distributed environment."""
